600123233ce8273f0ce80160f136af2b9ad215b2
Dolon Roy <dolonr718@gmail.com>
2023-10-08 20:28:39 +0600

    dynamic web title added commit

--- src/All-JSX/Layout/MainLayout.jsx
+++ src/All-JSX/Layout/MainLayout.jsx
@@ -1,12 +1,21 @@
 
 import "swiper/css";
 import "./MainLayout.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Componentes/Footer/Footer";
+import { useEffect } from "react";
 
 
 const MainLayout = () => {
+    const location = useLocation();
 
+    useEffect(() => {
+        if(location.pathname === "/") {
+            document.title = "Genesis | Home"
+            return;
+        }
+        document.title = "Genesis " + location.pathname.replace("/", "| ");
+    }, [location.pathname])
 
     return (
         <div className="overflow-hidden">
